2ab6db96c4cd1a19f3d437078672e3053257182e
clemencegc <clemence.giraudch@gmail.com>
2022-10-30 16:16:10 +0100

    fix back ticks

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
-    hours = "0${hours}";
+    hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    minutes = "0${minutes}";
+    minutes = `0${minutes}`;
   }
   let days = [
     "Sunday",
@@ -18,7 +18,7 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let day = days[date.getDay()];
-  return "${day} ${hours}:${minutes}";
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
@@ -40,15 +40,14 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    "http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png"
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function search(city) {
   let apiKey = "2e5b6f97a330658a59719220e02b26a0";
-  let apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2e5b6f97a330658a59719220e02b26a0&units=metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2e5b6f97a330658a59719220e02b26a0&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
